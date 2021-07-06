73b3230088fe4d9305d54595d15945484040a966
rayhankinan <rayhankinan@gmail.com>
2021-07-06 23:52:36 +0700

    GetData udh bisa

--- GenerateEmail.py
+++ GenerateEmail.py
@@ -8,3 +8,12 @@ def GetData(**kwargs):
             url += '?' + str(key) + '=' + str(value)
         else:
             url += "&" + str(key) + '=' + str(value)
+    
+    response = requests.request("GET", url)
+
+    data = response.json()
+
+    return data
+
+if __name__ == "__main__":
+    pass
